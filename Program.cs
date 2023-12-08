fa59b1361ad9580f50dc0c9d7741f5befa6a81ee
Colin Matteson <seefm@proton.me>
2023-12-08 13:31:52 -0500

    guessing game final

--- Program.cs
+++ Program.cs
@@ -61,20 +61,6 @@ bool GuessNumber ()
         userAnswer = stringnum;
         return false;
     }
-    // while (answer != "y" && answer != "n")
-    // {
-    //     Console.Write($"{question} (Y/N): ");
-    //     answer = Console.ReadLine().ToLower();
-    // }
-
-    // if (answer == "y")
-    // {
-    //     return true;
-    // }
-    // else
-    // {
-    //     return false;
-    // }
 };
 
 void Cheater ()
@@ -177,74 +163,6 @@ void MoreGuesses ()
     }
 }
 
-// void MooseSays(string message)
-// {
-//     Console.WriteLine();
-// }
-
-// void Questions () 
-// {
-//     List<string> AIQuestions = new List<string>() { "Is Canada real?", "You like food tho?", "You got a car or are you out here strugglin?"};
-//     List<string> TrueAnswers = new List<string>() { "Really? It seems very unlikely", "This is the only correct answer", "damn ok I see you mr. monopoly money bags MF"};
-//     List<string> FalseAnswers = new List<string>() { "I K N E W I T!!!!!!!", "wtf who hurt you bro?", "ouch it's like they say the strugg is rill"};
-
-//     for (int i = 0; i < 3; i++)
-//         {
-//            bool isTrue = MooseAsks(AIQuestions[i]);
-//            if (isTrue)
-//            {
-//             MooseSays(TrueAnswers[i]);
-//            }
-//            else 
-//            {
-//             MooseSays(FalseAnswers[i]);
-//            }
-//         }
-
-// }
-
-//void CanadaQuestion ()
-//{
-//    bool isTrue = MooseAsks("Is Canada real?");
-//    if (isTrue)
-//    {
-//        MooseSays("Really? It seems very unlikely.");
-//    }
-//    else
-//    {
-//        MooseSays("I  K N E W  I T !!!");
-//    }
-//}
-//
-//void FoodQuestion ()
-//{
-//    bool isTrue = MooseAsks("You like food tho?");
-//    if (isTrue)
-//    {
-//        MooseSays("this is the only correct answer");
-//    }
-//    else
-//    {
-//        MooseSays("wtf who hurt you bro");
-//    }
-//}
-//
-//void CarQuestion ()
-//{
-//    bool isTrue = MooseAsks("You got a car or are you out here strugglin?");
-//    if (isTrue)
-//    {
-//        MooseSays("damn ok flex mr. monopoly money bag mf");
-//    }
-//    else
-//    {
-//        MooseSays("ouch");
-//    }
-//}
-//
-//CanadaQuestion();
-//FoodQuestion();
-//CarQuestion();
 
 Difficulty();
 
